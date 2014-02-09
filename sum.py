eb2ff69ce936acca10b7eecc97c59981a48beba1
Jeremy Brudvik <jeremy@brudvik.me>
2014-02-09 16:45:17 +0100

    Write sum to a new file

--- sum.py
+++ sum.py
@@ -2,4 +2,5 @@ import sublime, sublime_plugin
 
 class SumCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    self.view.insert(edit, 0, '42')
+    new_view = self.view.window().new_file()
+    new_view.insert(edit, 0, '42')
