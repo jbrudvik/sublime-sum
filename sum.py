f6375421c59125888dced9cda4306f73ee2088c5
Jeremy Brudvik <jeremy@brudvik.me>
2014-02-09 17:00:24 +0100

    Added name ('Sum') to new file

--- sum.py
+++ sum.py
@@ -3,4 +3,5 @@ import sublime, sublime_plugin
 class SumCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     new_view = self.view.window().new_file()
+    new_view.set_name('Sum')
     new_view.insert(edit, 0, '42')
