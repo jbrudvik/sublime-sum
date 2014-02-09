f60fdf17e2c91891c32e8a8d801f0614576fd765
Jeremy Brudvik <jeremy@brudvik.me>
2014-02-09 18:23:17 +0100

    Echo file contents to new file

--- sum.py
+++ sum.py
@@ -4,6 +4,9 @@ class SumCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         sum_view = self.view.window().new_file()
         sum_view.set_name('Sum')
-        sum_view.insert(edit, 0, '42')
+
+        file_text = self.view.substr(sublime.Region(0, self.view.size()))
+        sum_view.insert(edit, 0, file_text)
+
         sum_view.set_read_only(True)
         sum_view.set_scratch(True)
