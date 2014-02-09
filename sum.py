e4cbc49d0ce3f356cf4e7338bb355e38e6d31098
Jeremy Brudvik <jeremy@brudvik.me>
2014-02-09 17:54:10 +0100

    Conform to PEP-8: Use 4-space indents

--- sum.py
+++ sum.py
@@ -1,9 +1,9 @@
 import sublime, sublime_plugin
 
 class SumCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    sum_view = self.view.window().new_file()
-    sum_view.set_name('Sum')
-    sum_view.insert(edit, 0, '42')
-    sum_view.set_read_only(True)
-    sum_view.set_scratch(True)
+    def run(self, edit):
+        sum_view = self.view.window().new_file()
+        sum_view.set_name('Sum')
+        sum_view.insert(edit, 0, '42')
+        sum_view.set_read_only(True)
+        sum_view.set_scratch(True)
