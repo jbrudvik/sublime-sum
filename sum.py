4bb67d6b06042019df4d06328eabe8b06d5dc15f
Jeremy Brudvik <jeremy@brudvik.me>
2014-02-09 17:18:18 +0100

    Change `new_view` to `sum_view`
    `sum_view` is a more descriptive variable name.

--- sum.py
+++ sum.py
@@ -2,8 +2,8 @@ import sublime, sublime_plugin
 
 class SumCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    new_view = self.view.window().new_file()
-    new_view.set_name('Sum')
-    new_view.insert(edit, 0, '42')
-    new_view.set_read_only(True)
-    new_view.set_scratch(True)
+    sum_view = self.view.window().new_file()
+    sum_view.set_name('Sum')
+    sum_view.insert(edit, 0, '42')
+    sum_view.set_read_only(True)
+    sum_view.set_scratch(True)
