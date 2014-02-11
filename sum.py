deb6255df99f3ae8c7005162a42db1bb22681abf
Jeremy Brudvik <jeremy@brudvik.me>
2014-02-10 16:09:42 -0800

    Disregard: , {} [] ()

--- sum.py
+++ sum.py
@@ -1,4 +1,5 @@
 import sublime, sublime_plugin
+import re
 
 class SumCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -6,11 +7,11 @@ class SumCommand(sublime_plugin.TextCommand):
         sum_view.set_name('Sum')
 
         file_text = self.view.substr(sublime.Region(0, self.view.size()))
-        numbers = [to_number(s) for s in file_text.split() if is_number(s)]
+        words = [s for s in re.split('[\s,[\](){}]', file_text)]
+        numbers = [to_number(s) for s in words if is_number(s)]
         result = sum(numbers)
 
         sum_view.insert(edit, 0, str(result))
-
         sum_view.set_read_only(True)
         sum_view.set_scratch(True)
 
