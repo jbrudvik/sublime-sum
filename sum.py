5024c324cd1d25d97a90bdca8e095008593a3515
Jeremy Brudvik <jeremy@brudvik.me>
2014-02-16 21:17:04 -0800

    Strip leading and trailing currency symbols

--- sum.py
+++ sum.py
@@ -8,12 +8,26 @@ class SumCommand(sublime_plugin.TextCommand):
 
         file_text = self.view.substr(sublime.Region(0, self.view.size()))
         words = [s for s in re.split('[\s,[\](){}]', file_text)]
-        numbers = [to_number(s) for s in words if is_number(s)]
+        stripped_words = [strip_currency(s) for s in words]
+        numbers = [to_number(s) for s in stripped_words if is_number(s)]
         result = sum(numbers)
 
         sum_view.insert(edit, 0, str(result))
         sum_view.set_read_only(True)
         sum_view.set_scratch(True)
+
+def strip_currency(s):
+    """
+    Return a string without leading or trailing currency symbols
+
+    Strip leading and trailing currency symbols, and currency symbols
+    at position 1 if first character is + or -
+    """
+    currency_symbols = '$€£¥¢'
+    s = s.strip(currency_symbols)
+    if len(s) > 2 and s[0] in '+-' and s[1] in currency_symbols:
+        s = s[0] + s[2:]
+    return s
 
 def is_int(s):
     """Return boolean indicating whether a string can be parsed to an int."""
