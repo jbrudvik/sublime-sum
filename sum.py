74f6b6738181528823f17b34d2ce6ee983f9c1d2
Jeremy Brudvik <jeremy@brudvik.me>
2014-12-16 02:22:19 -0800

    Ensure dirty indicator never shows in window init

--- sum.py
+++ sum.py
@@ -6,15 +6,15 @@ from utils import *
 
 class SumCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        # Create new buffer
+        # Create new buffer that never reports dirty status
         sum_view = self.view.window().new_file()
         sum_view.set_name('Sum')
+        sum_view.set_scratch(True)
 
         # Insert sum of numbers from original buffer into new buffer
         file_text = self.view.substr(sublime.Region(0, self.view.size()))
         file_sum = sum_of_numbers_in_string(file_text)
         sum_view.insert(edit, 0, str(file_sum))
 
-        # Make new buffer read-only and never report dirty status
+        # Make new buffer read-only
         sum_view.set_read_only(True)
-        sum_view.set_scratch(True)
